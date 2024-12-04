b26cba24e9a21ae5675f4951dfd15e2ba2ef3338
nhan2892005 <143471539+nhan2892005@users.noreply.github.com>
2024-12-04 11:53:24 +0700

    first commit

---
+++ __main__.py
@@ -0,0 +1,11 @@
+"""Entrance."""
+
+import env
+
+if __name__ == '__main__':
+    environment, scheduler = env.load()
+    while not environment.terminated():
+        environment.plot()
+        actions = scheduler.schedule()
+        print(actions)
+    print('END')
